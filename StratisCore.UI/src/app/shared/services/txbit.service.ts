971b5db54e9468e455a67296a5d7fcd0ec76f236
Kees de Wit <keesdewit@keesdewit.nl>
2019-10-25 16:03:16 +0200

    Change refresh rate of market txbit data

--- StratisCore.UI/src/app/shared/services/txbit.service.ts
+++ StratisCore.UI/src/app/shared/services/txbit.service.ts
@@ -9,13 +9,12 @@ import { startWith, switchMap} from 'rxjs/operators';
 })
 export class TxbitService {
   constructor(http: HttpClient) {
-    console.log("Construct txbit");
     this.http = http;
   }
 
   private apiBaseUrl: string = "https://api.txbit.io/api";
   private http: HttpClient;
-  private pollingInterval = interval(60 * 1000 * 30);
+  private pollingInterval = interval(5000);
 
   public getMarketSummary(): Observable<MarketSummary> {
     return this.pollingInterval.pipe(
